24e0c16aadf10c34ab1495e6b5532632f4e5b12e
Jukka Villstedt <jvillste@hotmail.com>
2012-06-29 23:57:37 +0300

    x

--- src/names/core.clj
+++ src/names/core.clj
@@ -59,8 +59,51 @@
   (is (= (extract-pattern "abc" [["a" "b"] ["c" "d"]])
          '(["a" "b"] ["a" "b"] ["c" "d"]))))
 
+(defn extract-pattern-signature [name letter-groups]
+  (map (fn [letter]
+         (first (filter (fn [index]
+                          (some (fn [group-letter]
+                                  (= (str letter) group-letter))
+                                (letter-groups index)))
+                        (range 0 (count letter-groups)))))
+       name))
+
+(defn increase-histogram-value [histogram key]
+  (if (contains? histogram key)
+    (update-in histogram [key] inc)
+    (update-in histogram [key] (fn [_] 1))))
+
+(deftest increase-histogram-value-test
+  (is (= (increase-histogram-value {} :a)
+         {:a 1}))
+  (is (= (increase-histogram-value {:a 4} :a)
+         {:a 5})))
+
+(defn pattern-signature-histogram [names letter-groups]
+  (reduce (fn [histogram name]
+            (increase-histogram-value histogram
+                                      (extract-pattern-signature name
+                                                                 letter-groups)))
+          {}
+          names))
+
+
+(deftest pattern-signature-histogram-test
+  (is (= (pattern-signature-histogram ["aa" "ab"] [["a"] ["b"]])
+         '{(0 1) 1, (0 0) 1})))
+
+(defn format-pattern-signature-histogram [histogram]
+  (for [key (reverse (sort-by histogram (keys histogram)))]
+    (str (vec key) " : " (histogram key))))
+
+(deftest format-pattern-signature-histogram-test
+  (is (= (format-pattern-signature-histogram (pattern-signature-histogram ["aa" "ab"] [["a"] ["b"]]))
+         '("[0 0] : 1" "[0 1] : 1"))))
+
+
+
 (defn spit-names [spec]
-  (spit "names.txt" (format-name-list 20 (names spec))))
+  (spit "names.txt" (format-name-list (int (/ 100 (count spec))) (names spec))))
 
 (run-tests)
 
@@ -70,9 +113,19 @@
 
   (let [v (remove #{"ä" "ö" "å"} vocals)
         c (remove #{"b" "c" "f" "q" "w" "x" "z"} consonants)]
-    (spit-names [v c c v]))
+    (spit-names ["kl" v c v c v]))
 
   (let [front-vocals '("i" "o" "u" "y")
         back-vocals '("a" "e")
         consonants '("d" "g" "h" "j" "k" "l" "m" "n" "p" "r" "s" "t" "v")]
-    (spit-names (extract-pattern "lumia" [front-vocals back-vocals consonants]))))
+    (spit-names (extract-pattern "eliel" [front-vocals back-vocals consonants])))
+  (spit-names (extract-pattern "esa" ['("i" "o" "u" "y")
+                                      '("a" "e")
+                                      '("h" "j" "s" "v")
+                                      '("d" "g" "k" "l" "m" "n" "p" "r" "t")]))
+  (spit-names (extract-pattern "klojure" ['("i" "o" "u" "y" "a" "e")
+                                          '("h" "j" "s" "v" "d" "g" "k" "l" "m" "n" "p" "r" "t")]))
+
+  (println (extract-pattern-signature "klojure" ['("i" "o" "u" "y" "a" "e")
+                                                 '("h" "j" "s" "v" "d" "g" "k" "l" "m" "n" "p" "r" "t")]))
+  )
